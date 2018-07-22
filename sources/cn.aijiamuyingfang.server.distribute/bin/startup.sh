dc4ac4b1646c95ff2af30e02c9f01255b19ce218
shiwei <shiweideyouxiang@sina.cn>
2018-07-22 18:54:36 +0800

    测试Jenkins部署

--- sources/cn.aijiamuyingfang.server.distribute/bin/startup.sh
+++ sources/cn.aijiamuyingfang.server.distribute/bin/startup.sh
@@ -7,7 +7,6 @@ else
 fi
 
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
-
 . $SHELL_FOLDER/commons.sh
 stopProcess cn.aijiamuyingfang.server.goods
 echo "nohup java -jar /root/weapp/services/goods/cn.aijiamuyingfang.server.goods-0.0.1-boot.jar $WEAPP_OPTS >/root/weapp/logs/goods.log 2>&1 &"
@@ -17,7 +16,7 @@ stopProcess cn.aijiamuyingfang.server.shopcart
 echo "nohup java -jar /root/weapp/services/shopcart/cn.aijiamuyingfang.server.shopcart-0.0.1-boot.jar $WEAPP_OPTS >/root/weapp/logs/shopcart.log 2>&1 &"
 nohup java -jar /root/weapp/services/shopcart/cn.aijiamuyingfang.server.shopcart-0.0.1-boot.jar $WEAPP_OPTS >/root/weapp/logs/shopcart.log 2>&1 &
 
-stopProcess cn.aijiamuyingfang.server.order
+stopProcess cn.aijiamuyingfang.server.shoporder
 echo "nohup java -jar /root/weapp/services/shoporder/cn.aijiamuyingfang.server.shoporder-0.0.1-boot.jar $WEAPP_OPTS >/root/weapp/logs/shoporder.log 2>&1 &"
 nohup java -jar /root/weapp/services/shoporder/cn.aijiamuyingfang.server.shoporder-0.0.1-boot.jar $WEAPP_OPTS >/root/weapp/logs/shoporder.log 2>&1 &
 
