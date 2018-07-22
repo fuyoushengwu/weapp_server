56a2b5fbb05b0440509e1d0a6907f27903777d12
shiwei <shiweideyouxiang@sina.cn>
2018-07-22 20:26:00 +0800

    测试Jenkins部署

--- sources/cn.aijiamuyingfang.server.wxservice/src/main/java/cn/aijiamuyingfang/server/wxservice/WXServiceApplication.java
+++ sources/cn.aijiamuyingfang.server.wxservice/src/main/java/cn/aijiamuyingfang/server/wxservice/WXServiceApplication.java
@@ -3,6 +3,7 @@ package cn.aijiamuyingfang.server.wxservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * [描述]:
@@ -16,6 +17,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
  * @date 2018-06-24 20:31:54
  */
 @SpringBootApplication(scanBasePackages = { "cn.aijiamuyingfang.server" })
+@EnableJpaRepositories(basePackages = { "cn.aijiamuyingfang.server" })
 @EntityScan(basePackages = { "cn.aijiamuyingfang.server" })
 public class WXServiceApplication {
   public static void main(String[] args) {
